80274e21e44f9aa80143782a771fd444174f89d1
Bruno Mariano <bsignx@gmail.com>
2022-01-17 21:34:10 -0300

    chore: config react-localization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import type { NextPage } from 'next'
 
 import { Thing } from '@bsignx/bravel-ui'
 
+import { strings } from '../translations'
+
 const Home: NextPage = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Thing />
+      {strings.test}
     </>
   )
 }
---
+++ src/translations/index.ts
@@ -0,0 +1,8 @@
+import LocalizedStrings from 'react-localization'
+
+import en from './en.json'
+import pt from './pt-BR.json'
+
+const strings = new LocalizedStrings({ en, pt })
+
+export { strings }

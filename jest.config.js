88ec9d960766ca94abd044ef56d5bab05a21ddb1
Bruno Mariano <bsignx@gmail.com>
2022-01-17 21:17:04 -0300

    chore(jest): add collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
   moduleNameMapper: {
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@pages(.*)$': '<rootDir>/src/pages$1',

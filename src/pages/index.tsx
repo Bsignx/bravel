0486dec8ecd53c280d13e8e6f533b4c4d2eb24a9
Bruno Mariano <bsignx@gmail.com>
2022-01-17 21:15:00 -0300

    chore: add bravel ui lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next'
 
+import { Thing } from '@bsignx/bravel-ui'
+
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Thing />
+    </>
+  )
 }
 
 export default Home

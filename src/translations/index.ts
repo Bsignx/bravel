3c63c3d66520a716a7a5be712f3eaaa7379c632c
Bruno Mariano <bsignx@gmail.com>
2022-01-17 21:38:18 -0300

    chore: rename import

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,7 +1,7 @@
 import LocalizedStrings from 'react-localization'
 
 import en from './en.json'
-import pt from './pt-BR.json'
+import pt from './pt-br.json'
 
 const strings = new LocalizedStrings({ en, pt })
 

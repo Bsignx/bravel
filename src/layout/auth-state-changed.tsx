1f1febd01f516bf40b1cfbd655470d6520b50276
Bruno Mariano <bsignx@gmail.com>
2022-01-19 21:02:22 -0300

    feat(auth): create auth state changed component

---
+++ src/layout/auth-state-changed.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode, useEffect, useState } from 'react'
+
+import { useAuth } from '../contexts/auth'
+import AuthService from '../services/auth'
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export const AuthStateChanged = ({ children }: AuthProviderProps) => {
+  const { setUser } = useAuth()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    AuthService.waitForUser((userCred) => {
+      setUser(userCred)
+      setLoading(false)
+    })
+  }, [setUser])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  return <>{children}</>
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from 'next'
 
+import { useAuth } from 'contexts/auth'
+
 const Login: NextPage = () => {
-  return <button>Google</button>
+  const { user, loginWithGoogle, error } = useAuth()
+
+  return (
+    <div>
+      {error && <h1>{error}</h1>}
+      <button onClick={loginWithGoogle}>Google</button>
+      <h1>{user?.uid}</h1>
+    </div>
+  )
 }
 
 export default Login

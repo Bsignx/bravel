3501092d7abf9980984f304fd3c86342665669f5
Bruno Mariano <bsignx@gmail.com>
2022-01-23 19:47:06 -0300

    chore: config bravel ui library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
-import { Thing } from '@bsignx/bravel-ui'
+import { ToggleSwitch } from '@bsignx/bravel-ui'
 import { strings } from '@translations/index'
 
 const Home: NextPage = () => {
+  const [checked, setChecked] = useState<boolean>(false)
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Thing />
+      <ToggleSwitch
+        enabled={checked}
+        onChange={(value: boolean) => setChecked(value)}
+      />
       {strings.test}
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  purge: ['./node_modules/@bsignx/bravel-ui/dist/*.js'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',

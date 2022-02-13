9c3eea6e4d3666c88db9afc6f7a6b101b52eaf62
Bruno Mariano <bsignx@gmail.com>
2022-02-12 21:41:37 -0300

    chore: config i18

--- src/translations/index.ts
+++
@@ -1,8 +0,0 @@
-import LocalizedStrings from 'react-localization'
-
-import en from './en.json'
-import pt from './pt-br.json'
-
-const strings = new LocalizedStrings({ en, pt })
-
-export { strings }

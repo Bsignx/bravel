764e595e69f0a53805df45b827e02848c52a8af2
Bruno Mariano <bsignx@gmail.com>
2022-03-16 16:13:49 -0300

    refactor: move and rename footer component

--- src/templates/landing/index.ts
+++ src/templates/landing/index.ts
@@ -1,6 +1,5 @@
 export * from './landing'
 export * from './landing-features'
-export * from './landing-footer'
 export * from './landing-groups-categories'
 export * from './landing-navbar'
 export * from './landing-hero'

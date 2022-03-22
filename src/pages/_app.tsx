ba5bd8f8a1b99011e944706315c2c86d2322a77d
Bruno Mariano <bsignx@gmail.com>
2022-03-22 10:01:42 -0300

    chore: add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 
 import '../configs/firebase-config'
 import { AuthProvider } from 'features/auth/auth-context'
@@ -9,10 +11,16 @@ import '../styles/globals.css'
 import '@bsignx/bravel-ui/dist/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <AuthProvider>
       <AuthStateChanged>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
       </AuthStateChanged>
     </AuthProvider>
   )
--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -1,18 +1,35 @@
 import { GetServerSideProps, NextPage } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { dehydrate, useQuery, QueryClient } from 'react-query'
 
 import { PageProps, withAuth } from 'features/auth/auth-route'
 import { DiscoverTemplate } from 'templates/discover'
 
+// update query and separate concerns
+
+const getGroups = (): Promise<any[]> =>
+  fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json').then(
+    (resp) => resp.json()
+  )
+
 const Discover: NextPage<PageProps> = ({ auth }) => {
   const { user, logout } = auth
+  const { data: groups } = useQuery('groups', getGroups, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  })
 
   return <DiscoverTemplate />
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery('groups', getGroups)
+
   return {
     props: {
+      dehydratedState: dehydrate(queryClient),
       ...(await serverSideTranslations(locale as string, ['landing-page'])),
     },
   }

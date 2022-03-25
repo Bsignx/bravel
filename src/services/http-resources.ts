9b72a9c1566f638c9291daa917f545dba6b07a48
Bruno Mariano <bsignx@gmail.com>
2022-03-25 09:59:41 -0300

    chore(http): create and apply base url

--- src/services/http-resources.ts
+++ src/services/http-resources.ts
@@ -1,15 +1,17 @@
 import { Groups, Profile } from '../domain'
 
+const BASE_URL = 'http://localhost:3000'
+
 const getGroups = (): Promise<Groups> =>
-  fetch('http://localhost:3000/groups').then((resp) => resp.json())
+  fetch(`${BASE_URL}/groups`).then((resp) => resp.json())
 
 const getProfile = (): Promise<Profile> =>
-  fetch('http://localhost:3000/profile').then((resp) => resp.json())
+  fetch(`${BASE_URL}/profile`).then((resp) => resp.json())
 
 const getEvents = (): Promise<Profile> =>
-  fetch('http://localhost:3000/events').then((resp) => resp.json())
+  fetch(`${BASE_URL}/events`).then((resp) => resp.json())
 
 const getCategories = (): Promise<Profile> =>
-  fetch('http://localhost:3000/events').then((resp) => resp.json())
+  fetch(`${BASE_URL}/categories`).then((resp) => resp.json())
 
 export { getGroups, getProfile, getEvents, getCategories }

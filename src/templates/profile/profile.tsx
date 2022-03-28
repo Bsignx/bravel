90befd6e10622e0e937265c7964c072ed5b71297
Bruno Mariano <bsignx@gmail.com>
2022-03-28 14:49:26 -0300

    chore(profile page): consume endpoint

--- src/templates/profile/profile.tsx
+++ src/templates/profile/profile.tsx
@@ -1,38 +1,52 @@
 import { Button, Typography } from '@bsignx/bravel-ui'
 import { Container } from '@components/container'
 import { Layout } from '@components/layout'
+import { Profile } from '@domain/index'
 
-export const ProfileTemplate = () => {
+type ProfileTemplateProps = {
+  profile: Profile | undefined
+}
+
+export const ProfileTemplate = ({ profile }: ProfileTemplateProps) => {
+  if (!profile) return null
+
+  const {
+    name,
+    url_avatar,
+    location,
+    attended_events,
+    groups_as_member,
+    created_at,
+  } = profile
+
   return (
     <Layout>
       <Container as="main" className="flex min-h-[43vh] justify-center">
         <section className="mb-32 flex">
           <div>
-            <img
-              src="https://avatars.githubusercontent.com/u/52089507?v=4"
-              className="w-32 rounded-full"
-              alt="Avatar"
-            />
+            <img src={url_avatar} className="w-32 rounded-full" alt={name} />
           </div>
 
           <div className="ml-9">
             <Typography variant="h3" className="font-medium !text-gray700">
-              Bruno Mariano
+              {name}
             </Typography>
             <Typography
               variant="body2"
               className="mt-1 font-medium !text-gray500"
             >
-              Member since January 2021
+              Member since {new Date(created_at).toLocaleDateString()}
             </Typography>
             <Typography variant="body2" className="mt-4 !text-gray700">
-              Barretos
+              {location}
             </Typography>
             <Typography variant="body2" className="!text-gray700">
-              Attended 0 Events
+              Attended {attended_events.length} Event
+              {attended_events.length > 1 ? 's' : ''}
             </Typography>
             <Typography variant="body2" className="!text-gray700">
-              Participate in 6 groups
+              Participate in {groups_as_member.length} group
+              {groups_as_member.length > 1 ? 's' : ''}
             </Typography>
             <Button className="mt-6" disabled>
               Edit profile

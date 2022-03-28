f960abf63d53ef2880391f3cabe42537cd782cf0
Bruno Mariano <bsignx@gmail.com>
2022-03-28 14:37:48 -0300

    chore(my groups page): consume endpoint

--- src/services/http-resources.ts
+++ src/services/http-resources.ts
@@ -1,12 +1,31 @@
 import { Group, Groups, Profile } from '../domain'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:8080'
 
 const getGroups = (): Promise<Groups> =>
   fetch(`${BASE_URL}/groups`).then((resp) => resp.json())
 
 const getGroup = (id: string): Promise<Group> =>
   fetch(`${BASE_URL}/groups/${id}`).then((resp) => resp.json())
+
+export type MyGroups = {
+  asOrganizer: Groups
+  asMember: Groups
+}
+
+const getMyGroups = async (): Promise<MyGroups> => {
+  const [groups, profile] = await Promise.all([getGroups(), getProfile()])
+
+  const asOrganizer =
+    groups.filter((group) => group.organized_by.id === profile.id) ?? []
+
+  const asMember =
+    groups.filter((group) =>
+      group.group_members.find((member) => member.id === profile.id)
+    ) ?? []
+
+  return { asOrganizer, asMember }
+}
 
 const getProfile = (): Promise<Profile> =>
   fetch(`${BASE_URL}/profile`).then((resp) => resp.json())
@@ -17,4 +36,11 @@ const getEvents = (): Promise<Profile> =>
 const getCategories = (): Promise<Profile> =>
   fetch(`${BASE_URL}/categories`).then((resp) => resp.json())
 
-export { getGroups, getProfile, getEvents, getCategories, getGroup }
+export {
+  getGroups,
+  getProfile,
+  getEvents,
+  getCategories,
+  getGroup,
+  getMyGroups,
+}

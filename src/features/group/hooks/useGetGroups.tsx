c009d7536da59f429092ebd8fd9fd2b3c377d4e4
Bruno Mariano <bsignx@gmail.com>
2022-03-30 15:52:07 -0300

    feat(navbar component): add search bar feature

--- src/features/group/hooks/useGetGroups.tsx
+++ src/features/group/hooks/useGetGroups.tsx
@@ -3,8 +3,12 @@ import { useQuery } from 'react-query'
 import { Groups } from '@domain/index'
 import { getGroups } from '@services/http-resources'
 
-export const useGetGroups = () =>
-  useQuery<Groups>('groups', () => getGroups(), {
+type UseGetGroups = {
+  searchText?: string
+}
+
+export const useGetGroups = ({ searchText = '' }: UseGetGroups = {}) =>
+  useQuery<Groups>('groups', () => getGroups({ searchText }), {
     refetchOnMount: false,
     refetchOnWindowFocus: false,
   })

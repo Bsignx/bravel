50683ddfe5f2d72bb60925cba7c9142ed40f6789
Bruno Mariano <bsignx@gmail.com>
2022-04-01 09:01:34 -0300

    chore: navbar and welcome page adjustments

---
+++ src/pages/my-events.tsx
@@ -0,0 +1,24 @@
+import { GetServerSideProps, NextPage } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import { Container } from '@components/container'
+import { Layout } from '@components/layout'
+import { PageProps, withAuth } from 'features/auth/auth-route'
+
+const MyEvents: NextPage<PageProps> = ({ auth }) => {
+  return (
+    <Layout>
+      <Container className="min-h-[42.8vh] pb-28">Todo</Container>
+    </Layout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ['landing-page'])),
+    },
+  }
+}
+
+export default withAuth(MyEvents)
--- src/templates/my-groups/my-groups.tsx
+++ src/templates/my-groups/my-groups.tsx
@@ -3,54 +3,6 @@ import { Container } from '@components/container'
 import { Layout } from '@components/layout'
 import { MyGroups } from '@services/http-resources'
 
-const GROUPS_MEMBER = [
-  {
-    id: '1',
-    name: 'Group 1',
-    description: 'Group 1 description',
-    image:
-      'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: '2',
-    name: 'Group 2',
-    description: 'Group 2 description',
-    image:
-      'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: '2',
-    name: 'Group 2',
-    description: 'Group 2 description',
-    image:
-      'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: '2',
-    name: 'Group 2',
-    description: 'Group 2 description',
-    image:
-      'https://images.unsplash.com/photo-1556817411-31ae72fa3ea0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-]
-
-const GROUPS_ORGANNIZER = [
-  {
-    id: '1',
-    name: 'Group 1',
-    description: 'Group 1 description',
-    image:
-      'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    id: '2',
-    name: 'Group 2',
-    description: 'Group 2 description',
-    image:
-      'https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-]
-
 type MyGroupsTemplateProps = {
   myGroups: MyGroups | undefined
 }
@@ -59,17 +11,24 @@ export const MyGroupsTemplate = ({ myGroups }: MyGroupsTemplateProps) => {
   if (!myGroups) {
     return null
   }
-
+  console.log(myGroups)
   const { asMember, asOrganizer } = myGroups
 
   return (
     <Layout>
-      <Container as="main" className="pb-32">
+      <Container as="main" className="min-h-[42.8vh] pb-28">
         <Typography variant="h3" className="font-medium !text-gray700">
           Your groups
         </Typography>
 
-        {asOrganizer.length > 0 && (
+        {!asMember.length && !asMember.length && (
+          <Typography variant="body2" className="text-gray600 mt-6">
+            You are not a member or organizer of any group. You can create a new
+            group or join.
+          </Typography>
+        )}
+
+        {asOrganizer?.length > 0 && (
           <>
             <Typography
               variant="body1"
@@ -97,7 +56,7 @@ export const MyGroupsTemplate = ({ myGroups }: MyGroupsTemplateProps) => {
           </>
         )}
 
-        {asMember.length > 0 && (
+        {asMember?.length > 0 && (
           <>
             <div className="my-6 h-px w-full bg-stroke" aria-hidden />
             <Typography
